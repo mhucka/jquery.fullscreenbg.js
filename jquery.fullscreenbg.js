6c015a56bac084ff55189684241b3b029fb8f2a8
Michael Hucka <mhucka@caltech.edu>
2012-05-06 02:05:17 -0400

    Untabified.

--- jquery.fullscreenbg.js
+++ jquery.fullscreenbg.js
@@ -113,13 +113,13 @@
     // Resize image when it's first loaded.
     $(options.bgID).imagesLoaded(function($images, $proper, $broken) {
         var img = $proper[0];
-	if (! img.width || ! img.height) return;
-	options.width = img.width;
-	options.height = img.height;
-	$(options.bgID).fullscreenbgResizer(options);
+        if (! img.width || ! img.height) return;
+        options.width = img.width;
+        options.height = img.height;
+        $(options.bgID).fullscreenbgResizer(options);
     });
     $(window).bind("resize", function() { 
-	$(options.bgID).fullscreenbgResizer(options); });
+        $(options.bgID).fullscreenbgResizer(options); });
     return this;
   };
 
@@ -129,27 +129,27 @@
 
     // Width-only option can be indicated in 2 ways:
     if (options.widthOnly
-	|| ($(options.bgID).attr('widthOnly') != null)) {
+        || ($(options.bgID).attr('widthOnly') != null)) {
       // Get browser window size.
       var browserwidth = $(window).width();
 
       // Scale the image width only.
       $(this).width(browserwidth);
       $(this).css('left', (browserwidth - $(this).width())/2);
-    } else {				// Both dimensions.
+    } else {                            // Both dimensions.
       // Get browser window size.
       var browserwidth = $(window).width();
       var browserheight = $(window).height();
-      var ratio = options.height / options.width;	
+      var ratio = options.height / options.width;       
 
       // Scale the image in both dimensions.
       var ratio = options.height / options.width;
       if ((browserheight/browserwidth) > ratio) {
-	$(this).height(browserheight);
-	$(this).width(browserheight / ratio);
+        $(this).height(browserheight);
+        $(this).width(browserheight / ratio);
       } else {
-	$(this).width(browserwidth);
-	$(this).height(browserwidth * ratio);
+        $(this).width(browserwidth);
+        $(this).height(browserwidth * ratio);
       }
 
       // Center the image.
@@ -157,6 +157,6 @@
       $(this).css('top', (browserheight - $(this).height())/2);
     }
 
-    return this; 		
+    return this;                
   };
  })(jQuery);
